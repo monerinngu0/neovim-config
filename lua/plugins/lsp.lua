2e75720a7aca6ec31306bde71deb27bfe9c42cfa
monerinngu <kaduki1125@gmail.com>
2026-05-14 19:45:39 +0900

    new

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,68 +1,75 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- 共通on_attach（キーマップ）
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
 
-            local on_attach = function(_, bufnr)
-                local opts = { buffer = bufnr }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
 
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-            end
+      -- C++
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--completion-style=detailed",
+          "--header-insertion=never",
+          "--pch-storage=memory",
+          "--function-arg-placeholders=false",
+        },
+      })
 
-            -- clangd
-            vim.lsp.config("clangd", {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                cmd = { "clangd", "--background-index" },
-            })
+      -- Python
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        cmd = { "pyright-langserver", "--stdio" },
+      })
 
-            -- Python
-            vim.lsp.config("pyright", {
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
+      --[[
+      -- Lua
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })]]--
 
-            -- Lua
-            vim.lsp.config("lua_ls", {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                    },
-                },
-            })
+      -- Rust
+      vim.lsp.config("rust_analyzer", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = true,
+          },
+        },
+      })
 
-            -- Rust
-            vim.lsp.config("rust_analyzer", {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = {
-                    ["rust-analyzer"] = {
-                        cargo = {
-                            allFeatures = true,
-                        },
-                        checkOnSave = true,
-                    },
-                },
-            })
-
-            -- 有効化
-            local servers = { "clangd", "pyright", "lua_ls", "rust_analyzer" }
-            for _, server in ipairs(servers) do
-                vim.lsp.enable(server)
-            end
-            
-        end,
-    },
+      -- 有効化
+      vim.lsp.enable("clangd")
+      vim.lsp.enable("pyright")
+      --vim.lsp.enable("lua_ls")
+      vim.lsp.enable("rust_analyzer")
+    end,
+  },
 }
